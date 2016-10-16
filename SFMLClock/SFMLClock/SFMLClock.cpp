1c819d7aaef4c95777a90b5d0318b7b4201c53e8
term96 <term96@yandex.com>
2016-10-16 03:58:24 +0300

    Добавлены цифры
    Цифры на циферблате
    Рефакторинг

--- SFMLClock/SFMLClock/SFMLClock.cpp
+++ SFMLClock/SFMLClock/SFMLClock.cpp
@@ -5,162 +5,261 @@
 ////////////////////////////////////////////////////////////
 #include "stdafx.h"
 
+#define _USE_MATH_DEFINES
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <ctime>
 #include <cmath>
+#include <string>
 
+const float CLOCK_CIRCLE_SIZE = 250;
+const float CLOCK_CIRCLE_THICKNESS = 2;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
-////////////////////////////////////////////////////////////
-/// Entry point of application
-///
-/// \return Application exit code
-///
-////////////////////////////////////////////////////////////
-int main()
+struct ClockHands
 {
-	// Define some variables and constants
-	static const int SCREEN_WIDTH = 800;
-	static const int SCREEN_HEIGHT = 600;
-	static const float PI = 3.1415927f;
-	static const float CLOCK_CIRCLE_SIZE = 250;
-	static const float CLOCK_CIRCLE_THICKNESS = 2;
-	float x, y;
-	float angle = 0.0;
+	sf::RectangleShape hourHand;
+	sf::RectangleShape minuteHand;
+	sf::RectangleShape secondsHand;
+};
 
-	// Set multisampling level
-	sf::ContextSettings settings;
-	settings.antialiasingLevel = 8;
+struct Clock
+{
+	sf::CircleShape centerCircle;
+	sf::CircleShape clockCircle; 
+	sf::CircleShape dots[60];
+};
 
-	// Create the window of the application
-	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML Analog Clock", sf::Style::Close, settings);
-
-	// Define windowCenter which gets the center of the window here, right after creating window
-	sf::Vector2f windowCenter = sf::Vector2f(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
-
-	// Create a list for clock's dots
-	sf::CircleShape dot[60];
-
-	// Create dots and place them to very right positions
-	for (int i = 0; i < 60; i++)
+int CreateDigits(sf::RenderWindow & window) {
+	sf::Font font;
+	if (!font.loadFromFile("resources/calibri.ttf"))
 	{
-		x = (CLOCK_CIRCLE_SIZE - 10) * cos(angle);
-		y = (CLOCK_CIRCLE_SIZE - 10) * sin(angle);
-
-		if (i % 5 == 0)
-			dot[i] = sf::CircleShape(3);
-		else
-			dot[i] = sf::CircleShape(1);
-		dot[i].setFillColor(sf::Color::Black);
-		dot[i].setOrigin(dot[i].getGlobalBounds().width / 2, dot[i].getGlobalBounds().height / 2);
-		dot[i].setPosition(x + window.getSize().x / 2, y + window.getSize().y / 2);
-
-		angle = angle + ((2 * PI) / 60);
+		return EXIT_FAILURE;
 	}
 
-	// Create outline of the clock
-	sf::CircleShape clockCircle(CLOCK_CIRCLE_SIZE);
+	float x, y;
+	double angle = -(2 * M_PI) / 6;
 
-	clockCircle.setPointCount(100);
-	clockCircle.setOutlineThickness(CLOCK_CIRCLE_THICKNESS);
-	clockCircle.setOutlineColor(sf::Color::Black);
-	clockCircle.setOrigin(clockCircle.getGlobalBounds().width / 2, clockCircle.getGlobalBounds().height / 2);
-	clockCircle.setPosition(window.getSize().x / 2 + CLOCK_CIRCLE_THICKNESS, window.getSize().y / 2 + CLOCK_CIRCLE_THICKNESS);
+	for (int i = 1; i < 13; i++)
+	{
+		x = static_cast<float>((CLOCK_CIRCLE_SIZE - 40) * cos(angle) - 2);
+		y = static_cast<float>((CLOCK_CIRCLE_SIZE - 40) * sin(angle) - 12);
 
-	// Crate another circle for center
-	sf::CircleShape centerCircle(10);
+		sf::Text text;
+		text.setFont(font);
+		text.setString(std::to_string(i));
+		text.setCharacterSize(36);
+		text.setFillColor(sf::Color::Black);
 
-	centerCircle.setPointCount(100);
-	centerCircle.setFillColor(sf::Color::Red);
-	centerCircle.setOrigin(centerCircle.getGlobalBounds().width / 2, centerCircle.getGlobalBounds().height / 2);
-	centerCircle.setPosition(windowCenter);
+		text.setOrigin(text.getGlobalBounds().width / 2, text.getGlobalBounds().height / 2);
+		text.setPosition(x + window.getSize().x / 2, y + window.getSize().y / 2);
 
-	// Create hour, minute, and seconds hands
-	sf::RectangleShape hourHand(sf::Vector2f(5, 180));
-	sf::RectangleShape minuteHand(sf::Vector2f(3, 240));
-	sf::RectangleShape secondsHand(sf::Vector2f(2, 250));
+		angle = angle + ((2 * M_PI) / 12);
 
-	hourHand.setFillColor(sf::Color::Black);
-	minuteHand.setFillColor(sf::Color::Black);
-	secondsHand.setFillColor(sf::Color::Red);
+		window.draw(text);
+	}
+	
+	return EXIT_SUCCESS;
+}
 
-	hourHand.setOrigin(hourHand.getGlobalBounds().width / 2, hourHand.getGlobalBounds().height - 25);
-	minuteHand.setOrigin(minuteHand.getGlobalBounds().width / 2, minuteHand.getGlobalBounds().height - 25);
-	secondsHand.setOrigin(secondsHand.getGlobalBounds().width / 2, secondsHand.getGlobalBounds().height - 25);
+// Create dots and place them to very right positions
+void CreateDots(Clock &clock, sf::RenderWindow const& window)
+{
+	float x, y;
+	double angle = 0.0;
 
-	hourHand.setPosition(windowCenter);
-	minuteHand.setPosition(windowCenter);
-	secondsHand.setPosition(windowCenter);
+	for (int i = 0; i < 60; i++)
+	{
+		x = static_cast<float>((CLOCK_CIRCLE_SIZE - 10) * cos(angle));
+		y = static_cast<float>((CLOCK_CIRCLE_SIZE - 10) * sin(angle));
 
-	// Create sound effect
-	sf::Music clockTick;
+		if (i % 5 == 0)
+			clock.dots[i] = sf::CircleShape(5);
+		else
+			clock.dots[i] = sf::CircleShape(2);
+		clock.dots[i].setFillColor(sf::Color::Black);
+		clock.dots[i].setOrigin(clock.dots[i].getGlobalBounds().width / 2, clock.dots[i].getGlobalBounds().height / 2);
+		clock.dots[i].setPosition(x + window.getSize().x / 2, y + window.getSize().y / 2);
+
+		angle = angle + ((2 * M_PI) / 60);
+	}
+}
+
+void InitClockCircle(Clock &clock, sf::RenderWindow const& window)
+{
+	clock.clockCircle.setPointCount(100);
+	clock.clockCircle.setOutlineThickness(CLOCK_CIRCLE_THICKNESS);
+	clock.clockCircle.setOutlineColor(sf::Color::Black);
+	clock.clockCircle.setOrigin(clock.clockCircle.getGlobalBounds().width / 2, clock.clockCircle.getGlobalBounds().height / 2);
+	clock.clockCircle.setPosition(window.getSize().x / 2 + CLOCK_CIRCLE_THICKNESS, window.getSize().y / 2 + CLOCK_CIRCLE_THICKNESS);
+}
+
+void InitCenterCircle(Clock &clock, sf::Vector2f const& windowCenter)
+{
+	clock.centerCircle.setPointCount(100);
+	clock.centerCircle.setFillColor(sf::Color::Black);
+	clock.centerCircle.setOrigin(clock.centerCircle.getGlobalBounds().width / 2, clock.centerCircle.getGlobalBounds().height / 2);
+	clock.centerCircle.setPosition(windowCenter);
+}
+
+void InitHands(ClockHands &clockHands, sf::Vector2f const& windowCenter)
+{
+	clockHands.hourHand.setFillColor(sf::Color::Black);
+	clockHands.minuteHand.setFillColor(sf::Color::Black);
+	clockHands.secondsHand.setFillColor(sf::Color::Red);
+
+	clockHands.hourHand.setOrigin(clockHands.hourHand.getGlobalBounds().width / 2, clockHands.hourHand.getGlobalBounds().height - 25);
+	clockHands.minuteHand.setOrigin(clockHands.minuteHand.getGlobalBounds().width / 2, clockHands.minuteHand.getGlobalBounds().height - 25);
+	clockHands.secondsHand.setOrigin(clockHands.secondsHand.getGlobalBounds().width / 2, clockHands.secondsHand.getGlobalBounds().height - 25);
+
+	clockHands.hourHand.setPosition(windowCenter);
+	clockHands.minuteHand.setPosition(windowCenter);
+	clockHands.secondsHand.setPosition(windowCenter);
+}
+
+int CreateSoundEffect(sf::Music & clockTick)
+{
 	if (!clockTick.openFromFile("resources/clock-1.wav"))
+	{
 		return EXIT_FAILURE;
+	}
 	clockTick.setLoop(true);
 	clockTick.play();
+	return EXIT_SUCCESS;
+}
 
-	// Use a part of SFML logo as clock brand
-	sf::Texture clockBrand;
+int CreateClockBrand(sf::Texture & clockBrand, sf::Sprite & clockBrandSprite, sf::RenderWindow & window)
+{
 	if (!clockBrand.loadFromFile("resources/clock-brand.png"))
 	{
 		return EXIT_FAILURE;
 	}
 
-	sf::Sprite clockBrandSprite;
 	clockBrandSprite.setTexture(clockBrand);
 	clockBrandSprite.setOrigin(clockBrandSprite.getTextureRect().left + clockBrandSprite.getTextureRect().width / 2.0f,
 		clockBrandSprite.getTextureRect().top + clockBrandSprite.getTextureRect().height / 2.0f);
 
-	clockBrandSprite.setPosition(window.getSize().x / 2.0f, window.getSize().y - 100.0f);
+	clockBrandSprite.setPosition(window.getSize().x / 2.0f, window.getSize().y - 150.0f);
+	return EXIT_SUCCESS;
+}
 
-
-	// Create clock background
-	sf::Texture clockImage;
-	if (!clockImage.loadFromFile("resources/clock-image.png"))
+int CreateClockBackground(sf::Texture & clockImage, Clock &clock)
+{
+	if (!clockImage.loadFromFile("resources/clock-image.jpg"))
 	{
 		return EXIT_FAILURE;
 	}
 
-	clockCircle.setTexture(&clockImage);
-	clockCircle.setTextureRect(sf::IntRect(40, 0, 500, 500));
+	clock.clockCircle.setTexture(&clockImage);
+	clock.clockCircle.setTextureRect(sf::IntRect(40, 0, 500, 500));
+	return EXIT_SUCCESS;
+}
+
+void HandleEvents(sf::RenderWindow & window)
+{
+	sf::Event event;
+	while (window.pollEvent(event))
+	{
+		// Window closed: exit
+		if (event.type == sf::Event::Closed)
+			window.close();
+	}
+}
+
+void RotateHands(ClockHands &clockHands)
+{
+	// Get system time
+	std::time_t currentTime = std::time(NULL);
+
+	struct tm * ptm = new tm;
+	localtime_s(ptm, &currentTime);
+
+	clockHands.hourHand.setRotation(ptm->tm_hour * 30.0f + (ptm->tm_min / 2));
+	clockHands.minuteHand.setRotation(ptm->tm_min * 6.0f + (ptm->tm_sec / 10));
+	clockHands.secondsHand.setRotation(ptm->tm_sec * 6.0f);
+}
+
+void DrawClock(sf::RenderWindow & window, sf::Sprite & clockBrandSprite, ClockHands &clockHands, Clock &clock)
+{
+	// Clear the window
+	window.clear(sf::Color::White);
+
+	// Draw all parts of clock
+	window.draw(clock.clockCircle);
+
+	for (int i = 0; i<60; i++)
+	{
+		window.draw(clock.dots[i]);
+	}
+
+	window.draw(clockBrandSprite);
+	window.draw(clockHands.hourHand);
+	window.draw(clockHands.minuteHand);
+	window.draw(clockHands.secondsHand);
+	window.draw(clock.centerCircle);
+}
+
+int main()
+{
+	// Set multisampling level
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML Analog Clock", sf::Style::Close, settings);
+		
+	// Define windowCenter which gets the center of the window here, right after creating window
+	sf::Vector2f windowCenter = sf::Vector2f(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
+	
+	Clock clock;
+	clock.centerCircle = sf::CircleShape(10);
+	clock.clockCircle = sf::CircleShape(CLOCK_CIRCLE_SIZE);
+
+	// Create a list for clock's dots
+	CreateDots(clock, window);
+
+	// Create outline of the clock
+	InitClockCircle(clock, window);
+	
+	// Crate another circle for center
+	InitCenterCircle(clock, windowCenter);
+	
+	// Create hour, minute, and seconds hands
+	ClockHands clockHands;
+	clockHands.hourHand = sf::RectangleShape(sf::Vector2f(5, 180));
+	clockHands.minuteHand = sf::RectangleShape(sf::Vector2f(3, 240));
+	clockHands.secondsHand = sf::RectangleShape(sf::Vector2f(2, 250));
+	InitHands(clockHands, windowCenter);
+
+	sf::Music clockTick;
+	if (CreateSoundEffect(clockTick) == EXIT_FAILURE)
+	{
+		return EXIT_FAILURE;
+	}
+
+	sf::Texture clockBrand;
+	sf::Sprite clockBrandSprite;
+	if (CreateClockBrand(clockBrand, clockBrandSprite, window) == EXIT_FAILURE)
+	{
+		return EXIT_FAILURE;
+	}
+
+	sf::Texture clockImage;
+	if (CreateClockBackground(clockImage, clock) == EXIT_FAILURE)
+	{
+		return EXIT_FAILURE;
+	}
 
 	while (window.isOpen())
 	{
-		// Handle events
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// Window closed: exit
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+		HandleEvents(window);
 
-		// Get system time
-		std::time_t currentTime = std::time(NULL);
+		RotateHands(clockHands);
 
-		struct tm * ptm = new tm;
-		localtime_s(ptm, &currentTime);
+		DrawClock(window, clockBrandSprite, clockHands, clock);
 
-		hourHand.setRotation(ptm->tm_hour * 30.0f + (ptm->tm_min / 2));
-		minuteHand.setRotation(ptm->tm_min * 6.0f + (ptm->tm_sec / 10));
-		secondsHand.setRotation(ptm->tm_sec * 6.0f);
-
-		// Clear the window
-		window.clear(sf::Color::White);
-
-		// Draw all parts of clock
-		window.draw(clockCircle);
-
-		for (int i = 0; i<60; i++)
-		{
-			window.draw(dot[i]);
-		}
-
-		window.draw(clockBrandSprite);
-		window.draw(hourHand);
-		window.draw(minuteHand);
-		window.draw(secondsHand);
-		window.draw(centerCircle);
+		CreateDigits(window);
 
 		// Display things on screen
 		window.display();
